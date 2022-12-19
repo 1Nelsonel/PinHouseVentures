aaf855c0bb4b62852622e7799de025a2bb09ea9c
1Nelsonel <nelsonmokoro37@gmail.com>
2022-12-19 13:11:38 +0300

    success

--- base/views.py
+++ base/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {}
-    return render(request, 'home.html', context)
+    return render(request, 'templates/home.html', context)
