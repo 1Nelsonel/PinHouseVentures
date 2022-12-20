2579f6db69f735970cec88d22c224aaf8bede055
1Nelsonel <nelsonmokoro37@gmail.com>
2022-12-20 04:50:09 +0300

    success

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('faqs', views.faqs, name='faqs')
+    path('about/', views.about, name='about'),
+    path('faqs/', views.faqs, name='faqs')
 ]
