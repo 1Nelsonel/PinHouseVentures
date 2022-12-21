71a19cab2354cc3f25429b3f331204a07bb7f077
1Nelsonel <nelsonmokoro37@gmail.com>
2022-12-21 11:00:16 +0300

    success

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('faqs/', views.faqs, name='faqs')
+    path('faqs/', views.faqs, name='faqs'),
+    path('agents/', views.agents, name='agents'),
+    path('agent/', views.agent, name='agent'),
+    path('listings/', views.listings, name='listings'),
+    path('listing/', views.listing, name='listing'),
+    path('blogs/', views.blogs, name='blogs'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
 ]
--- base/views.py
+++ base/views.py
@@ -12,3 +12,31 @@ def about(request):
 def faqs(request):
     context = {}
     return render(request, 'base/faqs.html', context)
+
+def agents(request):
+    context = {}
+    return render(request, 'base/agents.html', context)
+
+def agent(request):
+    context = {}
+    return render(request, 'base/agent.html', context)
+
+def listings(request):
+    context = {}
+    return render(request, 'base/listings.html', context)
+
+def listing(request):
+    context = {}
+    return render(request, 'base/listing.html', context)
+ 
+def blogs(request):
+    context = {}
+    return render(request, 'base/blogs.html', context)
+
+def blog(request):
+    context = {}
+    return render(request, 'base/blog.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'base/contact.html', context)
