80e5dd118d8d7ecbd8c63de619120b1079fe17a8
1Nelsonel <nelsonmokoro37@gmail.com>
2023-01-18 08:58:56 +0300

    success admin

--- PinHouseVentures/urls.py
+++ PinHouseVentures/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('Admin/', include('adminSection.urls'))
+
 ]
 
 if settings.DEBUG:
