b364d88ddb900e9780b1be3df788a307640e337d
1Nelsonel <nelsonmokoro37@gmail.com>
2023-01-30 16:28:32 +0300

    success

--- adminSection/urls.py
+++ adminSection/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
+    path('add-proerty/', views.addProperty, name="add-property"),
+    path('bookings/', views.bookings, name="bookings"),
+    path('my-property/', views.myProperty, name="my-property"),
+    path('messages/', views.messages, name="messages"),
+    path('my-profile', views.myProfile, name="my-profile")
 ]
--- adminSection/views.py
+++ adminSection/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from base.models import Property, Agent, Location, Propertycategory, Blog, Comment
 
 # Create your views here.
-
+# @login_required(login_url='login')
 def dashboard(request):
     context = {}
     return render(request, 'adminSection/dashboard.html', context)
+
+def addProperty(request):
+    locations = Location.objects.all()
+    context = {'locations': locations}
+    return render(request, 'adminSection/submit-property.html', context)  
+
+def bookings(request):
+    context = {}
+    return render(request, 'adminSection/bookings.html', context) 
+
+def myProperty(request):
+    properties = Property.objects.all()
+    context = {'properties': properties}
+    return render(request, 'adminSection/my-properties.html', context) 
+
+def myProfile(request):
+    context = {}
+    return render(request, 'adminSection/my-profile.html', context) 
+
+def messages(request):
+    context = {}
+    return render(request, 'adminSection/messages.html', context) 
